c7634d648462340ec1599c6d132f8faef6d7fc51
araohs <smlkts@gmail.com>
2015-10-30 01:32:52 +0900

    added maven project "hsoh-simple-app"

---
+++ hsoh-simple-app/src/main/java/me/hsoh/SimpleApplication.java
@@ -0,0 +1,13 @@
+package me.hsoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SimpleApplication.class, args);
+	}
+
+}
